d49215ba6a5d20e2530ffa53776d38a9ccb6500a
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-09-25 20:19:41 -0300

    outras mudanças no código

--- src/Candidatura.java
+++ src/Candidatura.java
@@ -1,11 +1,43 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Candidatura {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
+        String [] candidatos = {"Felipe", "Márcia", "Júlia", "Paulo", "Augusto"};
+        for(String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
         // analisarCandidato(1900.0);
         // analisarCandidato(2200.0);
         // analisarCandidato(2000.0);
-        selecaoCandidato();
+        //selecaoCandidato();
+
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continua = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continua = !atendeu;
+            if (continua){
+                tentativasRealizadas ++;
+            }else{
+                System.out.println("Contato realizado com sucesso!");
+            }
+        }while(continua && tentativasRealizadas < 3);
+            if(atendeu){
+                System.out.println("Conseguimos contato com "+ candidato + " na " + tentativasRealizadas + " tentativa");
+            }else{
+                System.out.println("não conseguimos contato com " + candidato  + " número máximo de tentativas " + tentativasRealizadas + " realizadas");
+            }
+        
+        
 
     }
     static void selecaoCandidato(){
@@ -13,6 +45,7 @@ public class Candidatura {
         int candidatoSelecionado = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
+
         while(candidatoSelecionado <5 && candidatoAtual < candidatos.length){
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
